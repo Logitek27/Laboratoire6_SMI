c7e90d0b189b905b085dd8a9f6d808d021fe67fd
Logitek27 <32298554+Logitek27@users.noreply.github.com>
2018-12-13 19:43:52 -0500

    Project Created

---
+++ Laboratoire 6/Inc/lcd.h
@@ -0,0 +1,19 @@
+/*
+ * lcd.h
+ *
+ *  Created on: 27 sept. 2018
+ *      Author: sebma
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void WriteCommand(int s32Command);
+void InitLCD(void);
+void WriteNumberLCD(char number);
+void WriteAsciiCharacterLCD(char asciiChar);
+void ClearLine2LCD(void);
+void WriteTimeLCD(unsigned int);
+
+
+#endif /* LCD_H_ */
